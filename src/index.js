7075d5eb70af1c13a2b737aef48a94d1a9d5fd89
sm-yury-y <sm.yuri-y@yandex.ru>
2019-04-01 22:18:29 +0300

    Добавляет API-клиент SwapiService

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+class SwapiService {
 
-getResource('https://swapi.co/api/people/1/')
-  .then((body) => {
-    console.log(body);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if(!res.ok) {
+      throw new Error(`Не удалось получить ${url}` + `, получил ${res.status}`)
+    }
+
+    return await res.json();
+  }
+
+   async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+   async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/palnets/${id}/`);
+  }
+
+   async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getPerson(3).then((p) => {
+  console.log(p.name);
+});
