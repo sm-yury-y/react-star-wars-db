7aeb2e33372df569ea91531cc3472f3860aa97d6
sm-yury-y <sm.yuri-y@yandex.ru>
2019-05-11 11:53:12 +0300

    Добавляет проверку значений свойств (props), которые получает компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -37,7 +37,7 @@ export default class App extends Component {
           <div className="container stardb-app">
             <Header onServiceChange={this.onServiceChange}/>
 
-              <RandomPlanet updateInterval = "2000"/>
+              <RandomPlanet />
 
               <PeoplePage />
               <PlanetsPage />
